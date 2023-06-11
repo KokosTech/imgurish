25393e223e86ec0c737168d56299f9a857f19acc
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-06-11 23:07:02 +0300

    [ADD] Delete post

---
+++ DeletePost/index.ts
@@ -0,0 +1,25 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { remove } from "../service/tableService";
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    const { board, id } = context.bindingData;
+
+    const entity = {
+      PartitionKey: { _: board },
+      RowKey: { _: id },
+    };
+
+    await remove("Posts", entity);
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: error.message,
+    };
+  }
+};
+
+export default httpTrigger;
